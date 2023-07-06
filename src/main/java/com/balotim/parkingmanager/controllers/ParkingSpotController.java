079b24307ddc05c833c87d26811918a5b1e6c910
Felipe Balotim <felipe.balotim@gmail.com>
2023-07-06 12:37:59 -0300

    feat: add get, put and delete methods, pagination and date format config

--- src/main/java/com/balotim/parkingmanager/controllers/ParkingSpotController.java
+++ src/main/java/com/balotim/parkingmanager/controllers/ParkingSpotController.java
@@ -6,12 +6,20 @@ import com.balotim.parkingmanager.services.ParkingSpotService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -22,10 +30,56 @@ public class ParkingSpotController {
 
     @PostMapping
     public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+        if (parkingSpotService.existsByLicensePlateCar(parkingSpotDto.getLicensePlateCar())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: License plate car is already in use.");
+        }
+        if (parkingSpotService.existsByParkingSpotNumber(parkingSpotDto.getParkingSpotNumber())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking spot is already in use.");
+        }
+        if (parkingSpotService.existsByApartmentAndBlock(parkingSpotDto.getApartment(), parkingSpotDto.getBlock())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking spot already registered for the apartment and block.");
+        }
         var parkingSpotModel = new ParkingSpotModel();
         BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
         parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
         return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpot(
+        @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)
+        Pageable pageable
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getById(@PathVariable(value = "id") UUID id) {
+        Optional<ParkingSpotModel> parkingSpotOptional = parkingSpotService.findById(id);
+        if (parkingSpotOptional.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found");
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotOptional.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id") UUID id) {
+        Optional<ParkingSpotModel> parkingSpotModel = parkingSpotService.findById(id);
+        if (parkingSpotModel.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found");
+        parkingSpotService.delete(parkingSpotModel.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Parking spot deleted successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParkingSpot(
+        @PathVariable(value = "id") UUID id,
+        @RequestBody @Valid ParkingSpotDto parkingSpotDto
+    ) {
+        Optional<ParkingSpotModel> parkingSpotOptional = parkingSpotService.findById(id);
+        if (parkingSpotOptional.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found");
+        var parkingSpotModel = parkingSpotOptional.get();
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setId(parkingSpotOptional.get().getId());
+        parkingSpotModel.setRegistrationDate(parkingSpotOptional.get().getRegistrationDate());
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
     }
 
 }
--- src/main/java/com/balotim/parkingmanager/services/ParkingSpotService.java
+++ src/main/java/com/balotim/parkingmanager/services/ParkingSpotService.java
@@ -3,7 +3,15 @@ package com.balotim.parkingmanager.services;
 import com.balotim.parkingmanager.models.ParkingSpotModel;
 import com.balotim.parkingmanager.repositories.ParkingSpotRepository;
 import jakarta.transaction.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,5 +23,30 @@ public class ParkingSpotService {
     @Transactional
     public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
         return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return parkingSpotRepository.findAll(pageable);
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
     }
 }
